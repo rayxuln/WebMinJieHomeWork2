f64763a9c6224e1ac0d1aab038c6fb7bf751db16
Raiix <imbosslx@sina.com>
2020-06-23 17:33:01 +0800

    Second commit

--- src/main/java/com/raiix/homework2/Poker.java
+++ src/main/java/com/raiix/homework2/Poker.java
@@ -1,13 +1,123 @@
 package com.raiix.homework2;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Poker {
+    int[] numberMap;
     String[] data;
 
     public Poker(String[] ps){
         data = ps;
+        numberMap = new int[256];
+        numberMap['2'] = 1;
+        numberMap['3'] = 2;
+        numberMap['4'] = 3;
+        numberMap['5'] = 4;
+        numberMap['6'] = 5;
+        numberMap['7'] = 6;
+        numberMap['8'] = 7;
+        numberMap['9'] = 8;
+        numberMap['T'] = 9;
+        numberMap['J'] = 10;
+        numberMap['Q'] = 11;
+        numberMap['K'] = 12;
+        numberMap['A'] = 13;
+    }
+
+    int getNumber(String p){
+        return numberMap[p.charAt(0)];
+    }
+
+    char getColor(String p){
+        return p.charAt(1);
+    }
+
+    boolean isSameNumber(String p1, String p2){
+        return getNumber(p1) == getNumber(p2);
+    }
+
+    boolean isInGreaterOrder(String p1, String p2){
+        return getNumber(p1) - getNumber(p2) == -1;
+    }
+    boolean isInLesserOrder(String p1, String p2){
+        return getNumber(p1) - getNumber(p2) == 1;
+    }
+
+    public boolean isSameType(){
+        char color = getColor(data[0]);
+        for (int i=1;i<data.length;++i){
+            if(color != getColor(data[i])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean isOrderType(){
+        boolean isGreater = true;
+        boolean isLesser = true;
+        for (int i=0;i<data.length-1;++i){
+            if(!isInGreaterOrder(data[i], data[i+1])) isGreater = false;
+            if(!isInLesserOrder(data[i], data[i+1])) isLesser = false;
+        }
+        return isGreater || isLesser;
+    }
+
+    public boolean isTripleType(){
+        for(int i=0;i<data.length;++i){
+            for(int j=0;j<data.length;++j){
+                for(int k=0;k<data.length;++k){
+                    if(i==j || j==k || i==k) continue;
+                    if(isSameNumber(data[i], data[j]) && isSameNumber(data[j], data[k]) && isSameNumber(data[i], data[k])){
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    public int countCoupleNum(){
+        HashSet<String> used = new HashSet<>();
+        int coupleCnt = 0;
+        for(int i=0;i<data.length;++i){
+            for(int j=0;j<data.length;++j){
+                if(i == j) continue;
+                if(used.contains(data[i]) || used.contains(data[j])) continue;
+                if(isSameNumber(data[i], data[j]))
+                {
+                    used.add(data[i]);
+                    used.add(data[j]);
+                    coupleCnt += 1;
+                }
+            }
+        }
+        return coupleCnt;
+    }
+
+    public boolean isCouple(){
+        return countCoupleNum() == 1;
+    }
+    public boolean isDoubleCouple(){
+        return countCoupleNum() == 2;
+    }
+    public boolean isChaos(){
+        return !(isSameType() || isOrderType() || isTripleType() || isCouple() || isDoubleCouple());
+    }
+
+    public boolean isSameOrderType(){
+        return isSameType() && isOrderType();
     }
 
     public String typeString(){
+        if(isSameType()) return "same";
+        if(isOrderType()) return "order";
+        if(isTripleType()) return "triple";
+
+        int coupleNum = countCoupleNum();
+        if(coupleNum == 1) return "couple";
+        if(coupleNum == 2) return "double couple";
         return "chaos";
     }
 }
--- src/test/java/com/raiix/homework2/AppTest.java
+++ src/test/java/com/raiix/homework2/AppTest.java
@@ -5,8 +5,81 @@ import org.junit.*;
 
 public class AppTest {
     @Test
-    public void givenSanPiAndPrintSanPi(){
+    public void givenSanPiAndPrint(){
         Poker p = new Poker(new String[]{"2H", "3D", "5S", "9C", "KD"});
-        assertEquals("chaos", p.typeString());
+        assertTrue(p.isChaos());
     }
+
+    @Test
+    public void givenDuiziAndPrint(){
+        Poker p = new Poker(new String[]{"2H", "2D", "5S", "9C", "KD"});
+        assertTrue(p.isCouple());
+    }
+
+    @Test
+    public void givenDuiziAndPrint2(){
+        Poker p = new Poker(new String[]{"TH", "TD", "5S", "9C", "KD"});
+        assertTrue(p.isCouple());
+    }
+
+    @Test
+    public void givenLiangDuiAndPrint(){
+        Poker p = new Poker(new String[]{"2H", "2D", "5S", "5C", "KD"});
+        assertTrue(p.isDoubleCouple());
+    }
+
+    @Test
+    public void givenLiangDuiAndPrint2(){
+        Poker p = new Poker(new String[]{"TH", "TD", "QS", "QC", "KD"});
+        assertTrue(p.isDoubleCouple());
+    }
+
+    @Test
+    public void givenSanTiaoAndPrintNotLiangDui(){
+        Poker p = new Poker(new String[]{"2H", "2D", "2S", "5C", "KD"});
+        assertFalse(p.isDoubleCouple());
+    }
+
+    @Test
+    public void givenSanTiaoAndPrint(){
+        Poker p = new Poker(new String[]{"2H", "2D", "2S", "2C", "KD"});
+        assertTrue(p.isTripleType());
+    }
+
+    @Test
+    public void givenDaShunZiAndPrint(){
+        Poker p = new Poker(new String[]{"2H", "3D", "4S", "5C", "6D"});
+        assertTrue(p.isOrderType());
+    }
+
+    @Test
+    public void givenDaShunZiAndPrint2(){
+        Poker p = new Poker(new String[]{"TH", "JD", "QS", "KC", "AD"});
+        assertTrue(p.isOrderType());
+    }
+
+    @Test
+    public void givenXiaoShunZiAndPrint(){
+        Poker p = new Poker(new String[]{"6H", "5D", "4S", "3C", "2D"});
+        assertTrue(p.isOrderType());
+    }
+
+    @Test
+    public void givenXiaoShunZiAndPrint2(){
+        Poker p = new Poker(new String[]{"7H", "8D", "9S", "TC", "JD"});
+        assertTrue(p.isOrderType());
+    }
+
+    @Test
+    public void givenTongHuaAndPrint(){
+        Poker p = new Poker(new String[]{"7H", "3H", "9H", "TH", "JH"});
+        assertTrue(p.isSameType());
+    }
+
+    @Test
+    public void givenTongHuaShunAndPrint(){
+        Poker p = new Poker(new String[]{"7H", "8H", "9H", "TH", "JH"});
+        assertTrue(p.isSameOrderType());
+    }
+
 }
