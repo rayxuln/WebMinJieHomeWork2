41a175cba54b588168aaebcd262930f380fa3f4d
Raiix <imbosslx@sina.com>
2020-06-23 16:41:21 +0800

    First commit

---
+++ src/main/java/com/raiix/homework2/Poker.java
@@ -0,0 +1,13 @@
+package com.raiix.homework2;
+
+public class Poker {
+    String[] data;
+
+    public Poker(String[] ps){
+        data = ps;
+    }
+
+    public String typeString(){
+        return "chaos";
+    }
+}
--- src/test/java/com/raiix/homework2/AppTest.java
+++ src/test/java/com/raiix/homework2/AppTest.java
@@ -4,5 +4,9 @@ import static org.junit.Assert.*;
 import org.junit.*;
 
 public class AppTest {
-
+    @Test
+    public void givenSanPiAndPrintSanPi(){
+        Poker p = new Poker(new String[]{"2H", "3D", "5S", "9C", "KD"});
+        assertEquals("chaos", p.typeString());
+    }
 }
